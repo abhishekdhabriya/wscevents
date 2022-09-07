bce5a9ed5b4ecb58c1509bcb2478a99b09ffcb3e
Abhishek Dhabriya <abhishek_dhabriya@intuit.com>
2022-09-07 14:24:39 -0700

    first commit

---
+++ src/test/java/com/srs/application/SrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srs.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
