0a3c689ca4688024960285bd7b6a1fbef26d1832
Abhishek Dhabriya <abhishek_dhabriya@intuit.com>
2022-09-12 17:29:34 -0700

    k8s deployment

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,10 @@ MAINTAINER A.D
 #Define default environment variables
 ENV SRS_HOME=/srs/home
 ENV SRS_BIN=/srs/srs/bin
+ENV APP_SPRING_DATASOURCE_HOST: mysql
+ENV APP_SPRING_PROFILES_ACTIVE: staging
+ENV APP_DB_NAME: srs_${APP_SPRING_PROFILES_ACTIVE}_db
+ENV APP_SECURITY_JWT_SECRET_KEY: srssecret
 
 # Create path
 RUN mkdir -p $SRS_BIN
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -e
 
-SPRING_PROFILES_ACTIVE=${SPRING_PROFILES_ACTIVE:-dev}
-
 # Marketplace core application with specific JVM_ARGS and SPRING_PROFILE
-java ${JVM_ARGS} -jar -Dspring.profiles.active=${SPRING_PROFILES_ACTIVE} ${SRS_HOME}/srs-v0.0.1.jar
+java ${JVM_ARGS} -jar -Dspring.profiles.active=${APP_SPRING_PROFILES_ACTIVE}  -Ddb_name=${APP_DB_NAME}  ${SRS_HOME}/srs-v0.0.1.jar
